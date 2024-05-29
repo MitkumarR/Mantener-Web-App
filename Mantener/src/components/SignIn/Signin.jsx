723843afb022edd658cf457ec00b1fbb2c6101e3
MitkumarR <147398468+MitkumarR@users.noreply.github.com>
2024-05-29 14:15:51 +0530

    Login changed to signin and signup added

---
+++ Mantener/src/components/SignIn/Signin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        <form action="">
+            <input type="text" />
+            <input type="text" />
+            
+        </form>
+    </div>
+  )
+}
+
+export default Login
