223acd2849a0e3ddeb23cc3a2ca791e816dfe261
MitkumarR <147398468+MitkumarR@users.noreply.github.com>
2024-05-29 14:13:06 +0530

    Login Page added

--- Mantener/src/App.jsx
+++ Mantener/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 
+import Navbar from './components/Navbar/Navbar'
+
 function App() {
   
 
   return (
     <>
-      
+      <Navbar/>  
     </>
   )
 }
--- Mantener/src/components/Navbar/Navbar.jsx
+++ Mantener/src/components/Navbar/Navbar.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-      
+    <div className='flex justify-center items-center text-white'>
+      <div className='Logo'>Mantener</div>
+      <input type="text" name='Searchbar' className='' placeholder='Search By Title'/><br/>
+      <button className='Refresh'>Refresh</button>
+      <button className='GridStyle'>List</button>
+      <div className='User'>User</div>
     </div>
   )
 }
