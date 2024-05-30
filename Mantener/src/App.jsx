222008b8fe5f8549cf6308a8ffab78c60285724a
MitkumarR <147398468+MitkumarR@users.noreply.github.com>
2024-05-30 22:50:44 +0530

    Update Signup.jsx
    give css and modify the some conditions

--- Mantener/src/App.jsx
+++ Mantener/src/App.jsx
@@ -15,21 +15,21 @@ function App() {
     <>
       {/* <Navbar/>  */}
       <Signup/>
-      <div>
+      {/* <div>
         <button
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
-          Increment
+          +
         </button>
         <span>{count}</span>
         <button
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
-          Decrement
+          -
         </button>
-      </div>
+      </div> */}
     </>
   )
 }
