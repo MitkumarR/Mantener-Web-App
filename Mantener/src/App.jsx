fab4e07d2c26b22b3d6ac7d1f43f1849eb4d92bf
MitkumarR <147398468+MitkumarR@users.noreply.github.com>
2024-05-30 16:47:01 +0530

    redux
    today i learn how to use of redux. with example increment decrement counter

--- Mantener/src/App.jsx
+++ Mantener/src/App.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './redux/counter/counterSlice'
+
 import Signup from './components/SignUp/Signup'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
   
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
 
   return (
     <>
       {/* <Navbar/>  */}
       <Signup/>
-      
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </>
   )
 }
