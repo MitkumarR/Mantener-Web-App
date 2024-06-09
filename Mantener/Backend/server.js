fb8e5ac7ecf9841e28ffea72612a11e1323e0f68
MitkumarR <147398468+MitkumarR@users.noreply.github.com>
2024-06-09 18:53:41 +0530

    getting some problem
    trying to fixing it

--- Mantener/Backend/server.js
+++ Mantener/Backend/server.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv')
+dotenv.config()
+
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+const dbName = 'Mantener';
 const app = express()
+
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+client.connect();
+
+app.get('/', async (req, res) => {
+  
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+  const findResult = await collection.find({}).toArray();
+  res.json(findResult);
 })
 
 app.listen(port, () => {
